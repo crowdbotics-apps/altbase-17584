6f98b76e6fdf3b0d8589f2af99dfafdbb6dee5f8
Crowdbotics <team@crowdbotics.com>
2020-05-30 18:28:23 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/altbase_17584/MainActivity.java
@@ -0,0 +1,15 @@
+package com.altbase_17584;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "altbase_17584";
+    }
+}
